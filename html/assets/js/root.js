79b0a2fe558ca0de460c52df0b16b22f7e36809f
victor <victormmla3lm@gmail.com>
2024-03-17 23:36:14 -0300

    Demo #2 - en progreso.

---
+++ html/assets/js/root.js
@@ -0,0 +1,17 @@
+let button = document.getElementById("changeTheme");
+const data = "data-site-theme";
+const lightIcon = "bi-sun-fill";
+const darkIcon = "bi-moon-stars-fill";
+
+button.addEventListener('click', () => {
+    let icon = button.getElementsByTagName('i')[0];
+    if (document.documentElement.dataset.siteTheme === "light") {
+        document.documentElement.setAttribute(data, "dark");
+        icon.classList.remove(lightIcon);
+        icon.classList.add(darkIcon);
+    } else {
+        document.documentElement.setAttribute(data, "light");
+        icon.classList.remove(darkIcon);
+        icon.classList.add(lightIcon);
+    }
+});
