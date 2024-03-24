4953031157f766409b0ff4feb3c9b2e20a63ca27
victor <victormmla3lm@gmail.com>
2024-03-23 23:24:30 -0300

    Demo #2 - contenido dinámico agregado, falta terminar el diseño.

--- html/assets/js/classes.js
+++ html/assets/js/classes.js
@@ -1,28 +1,36 @@
-export class PostInfo {
+export class PostCard {
 
+    category = undefined;
     title = undefined;
     author = undefined;
-    description = undefined;
     imgURL = undefined;
 
-    constructor(title, author, description, imgURL = "") {
+    constructor(title, author, category = "General", imgURL = "") {
         this.title = title;
         this.author = author;
-        this.description = description;
+        this.category = category;
         this.imgURL = imgURL;
     }
 
     body() {
         let body = document.createElement('div');
-        body.className = "post-info";
+        body.className = "card";
         let section = document.createElement('section');
         let img = document.createElement('img');
         img.src = this.imgURL;
         img.alt = "Imágen del post";
         section.appendChild(img);
+        let anchor = document.createElement('a');
+        anchor.href = "#";
+        anchor.className = "post-category";
+        anchor.innerText = this.category;
+        section.appendChild(anchor);
+        let anchorTitle = document.createElement('a');
+        anchorTitle.href = "#";
         let h4 = document.createElement('h4');
         h4.innerText = this.title;
-        section.appendChild(h4);
+        anchorTitle.appendChild(h4);
+        section.appendChild(anchorTitle);
         let small = document.createElement('small');
         small.innerHTML = this.author;
         section.appendChild(small);
@@ -30,4 +38,54 @@ export class PostInfo {
         return body;
     }
 
+}
+
+export class PostInfo extends PostCard {
+    date = undefined;
+    description = undefined;
+
+    constructor(title, author, category, description, imgURL = "", date = new Date()) {
+        super(title, author, category, imgURL);
+        this.date = date;
+        this.description = description;
+    }
+
+    body() {
+        let body = document.createElement('div');
+        body.className = "post-wrapper";
+        let article = document.createElement('article');
+        let anchor = document.createElement('a');
+        anchor.href = "#";
+        anchor.innerText = this.category;
+        article.appendChild(anchor);
+        let anchorTitle = document.createElement('a');
+        anchorTitle.href = "#";
+        let h3 = document.createElement('h3');
+        h3.innerHTML = this.title;
+        anchorTitle.appendChild(h3);
+        article.appendChild(anchorTitle);
+        let div1 = document.createElement('div');
+        let small = document.createElement('small');
+        small.innerHTML = this.author;
+        div1.appendChild(small);
+        let p = document.createElement('p');
+        p.className = "text-muted";
+        p.innerHTML = this.date;
+        div1.appendChild(p);
+        article.appendChild(div1);
+        let div2 = document.createElement('div');
+        let linkImg = document.createElement('a');
+        linkImg.href = "#";
+        let img = document.createElement('img');
+        img.src = this.imgURL;
+        img.alt = "Imágen del post";
+        linkImg.appendChild(img);
+        div2.appendChild(linkImg);
+        let paragraph = document.createElement('p');
+        paragraph.innerHTML = this.description;
+        div2.appendChild(paragraph);
+        article.appendChild(div2);
+        body.appendChild(article);
+        return body;
+    }
 }
--- html/assets/js/content-generator.js
+++ html/assets/js/content-generator.js
@@ -1,15 +1,23 @@
-import { PostInfo } from "./classes.js";
+import { PostCard, PostInfo } from "./classes.js";
 
-let totalInfoCards = 0;
-let topContainer = document.getElementById("top-content");
+let randomNumber = 0;
+let topContainer = document.getElementById("topContent");
+let mainContainer = document.getElementById("mainContent");
 
 // genera un número aleatorio entre el rango especificado
 const randomRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-totalInfoCards = randomRange(3, 6);
+randomNumber = randomRange(3, 6);
 
-for (let i = 0; i < totalInfoCards; i++) {
-    const post = new PostInfo("Post #" + (i + 1), "Autor");
-    topContainer.appendChild(post.body());
-    if (i < totalInfoCards - 1)
-        topContainer.appendChild(document.createElement('hr'));
+// agrego los posteos
+for (let i = 0; i < randomNumber; i++) {
+    const card = new PostCard("Post #" + (i + 1), "Autor", "General", "https://picsum.photos/200?random=" + randomRange(1, 100));
+    topContainer.appendChild(card.body());
+}
+
+// agrego contenido principal
+randomNumber = randomRange(1, 10);
+
+for (let i = 0; i < randomNumber; i++) {
+    const post = new PostInfo("Posteo del día", "Autor", "General", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus et amet praesentium sint cum neque, possimus sapiente at commodi error placeat rerum. Excepturi temporibus ab, aspernatur ea porro veritatis debitis.", "https://picsum.photos/200?random=" + randomRange(1, 100));
+    mainContainer.appendChild(post.body());
 }
